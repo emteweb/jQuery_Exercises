a580ec006540863c71a3279b39566245cbd403e0
Marcin Trebacz <emteweb@gmail.com>
2020-08-23 14:18:42 +0200

    jQuery basic methods

---
+++ methods.js
@@ -0,0 +1,27 @@
+// text(); html();
+
+//$("h1").text(); // return the content of an element (Vanilla JS => textContent )
+//$("h1").text("New text"); // changes the content of an element
+// $("ul").html(); // (VJS => innerHtml)
+
+// attr(); to access specific attribute / set specific attribute
+
+/* 
+$('img').css("width");
+"1931px"
+$('img').css("width", "300px");
+$("img").attr("src"); // returns the value of src
+$("img").attr("src", "https://images-na.ssl-images-amazon.com/images/I/71%2B1tRw991L._AC_SX466_.jpg");
+// with more elements
+$("img:first-of-type").attr("src", "https://..."); // changing the first element from the list
+$("img").last().attr("src", "https://..."); // // changing the last element from the list 
+*/
+
+// val(); returns the value of the selected element
+//$("input").val();
+
+// addClass(); removeClass(); toggleClass(); (VJS => classList.add)
+
+//$("h1").addClass("myClass");
+
+
